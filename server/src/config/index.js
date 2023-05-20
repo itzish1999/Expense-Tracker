39e1d606cec4da37a736db228920a975c9294578
itzish1999 <ismaeel.butt19@gmail.com>
2023-05-19 21:14:47 -0400

    feat(DB Connection): Connected to my postgres database

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -1,7 +1,20 @@
 require('dotenv').config();
 
+const { Pool } = require('pg')
+
+const dbConfig = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+};
+
+const pool = new Pool(dbConfig);
+
 const port = process.env.PORT || 7777;
 
 module.exports = {
-    port
+    port,
+    pool
 }
