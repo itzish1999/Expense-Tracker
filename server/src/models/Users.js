11e4df47ed6aa6b2d1c7495264931e50e13fbcb1
itzish1999 <ismaeel.butt19@gmail.com>
2023-05-20 01:01:57 -0400

    feat(Controller Model): Created the users model for the database.

---
+++ server/src/models/Users.js
@@ -0,0 +1,63 @@
+const { pool } = require('../config/index');
+
+class User {
+    constructor(id, name, email, password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    static async create(name, email, password) {
+        try {
+            const result = await pool.query(
+                'INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id',
+                [name, email, password]
+            )
+            const userId = result.rows[0].id;
+            return new User(userId, name, email, password);
+        } catch (error) {
+            throw new Error('Failed to create new user')
+        }
+    }
+
+    static async findById(id) {
+        try {
+            const result = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+            const user = result.rows[0];
+            user ? new User(user.id, user.name, user.email, user.password) : null;
+        } catch (error) {
+            throw new Error('Failed to find user by id');
+        }
+    }
+
+    static async findAll() {
+        try {
+            const result = await pool.query('SELECT * FROM users')
+            return result.rows.map((user) => new User(user.id, user.name, user.email, user.password));
+        } catch (error) {
+            throw new Error('Failed to get all users');
+        }
+    }
+
+    async update() {
+        try {
+            await pool.query(
+                'UPDATE users SET name = $1, email = $2, password = $3 WHERE id = $4',
+                [this.name, this.email, this.password]
+            );
+        } catch (error) {
+            throw new Error('Failed to update user');
+        }
+    }
+
+    async delete() {
+        try {
+            await pool.query('DELETE FROM users WHERE id = $1', [this.id]);
+        } catch (error) {
+            throw new Error('Failed to delete user');
+        }
+    }
+}
+
+module.exports = User;
