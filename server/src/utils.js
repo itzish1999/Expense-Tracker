63b1955da76e25bc837f75de9c018d08ece53019
itzish1999 <ismaeel.butt19@gmail.com>
2023-05-22 13:25:36 -0400

    feat(Accessibility): Made the functions accessible by all controllers that need to run the functions

--- server/src/utils.js
+++ server/src/utils.js
@@ -1,10 +1,10 @@
-export const isEmptyObject = (user) => {
-    return Object.keys(user).length !== 0 ? res.json(user) : res.status(404).json({ error: "User Not Found " });
+export const isEmptyObject = (obj) => {
+    return Object.keys(obj).length !== 0 ? res.json(obj) : res.status(404).json({ error: `${obj} not found` });
 };
 
-export const isUserExist = (user) => {
-    if (Object.keys(user).length !== 0) {
-        res.status(404).json({ error: 'User not found' });
+export const isExist = (obj) => {
+    if (Object.keys(obj).length !== 0) {
+        res.status(404).json({ error: `${obj} not found` });
         return;
     };
 }
